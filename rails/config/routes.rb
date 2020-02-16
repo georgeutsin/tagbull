c2a201cddb27f894286efb24d16e662a097fa175
George Utsin <georgeutsin@gmail.com>
2020-02-16 14:32:00 -0500

    rails: allow toggling activities enabled/disabled (#11)
    im a bad developer

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   scope module: :v1, defaults: { format: :json }, constraints: ApiConstraints.new(version: 1, default: true) do
     resource :activities, only: %i[show] do
-      get 'available'
+      get 'available', 'enable', 'disable'
     end
     resources :samples, only: %i[create]
     resources :projects, only: %i[index show update create delete] do
