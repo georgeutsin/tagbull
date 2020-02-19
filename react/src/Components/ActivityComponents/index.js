da9f52dda9524cbb6e564e9570ef7702e9534132
George Utsin <georgeutsin@gmail.com>
2020-02-19 17:02:32 -0500

    react: remove softcrop canvas (#22)
    * react: remove softcrop canvas
    
    * react: remove stale activities (#23)

--- react/src/Components/ActivityComponents/index.js
+++ react/src/Components/ActivityComponents/index.js
@@ -1,5 +1,2 @@
-export { default as AddLabel } from "./AddLabel";
-export { default as BoundingBox } from "./BoundingBox";
 export { default as BoundingBoxTap } from "./BoundingBoxTap";
 export { default as Locator } from "./Locator";
-export { default as BinaryVerification } from "./BinaryVerification";
