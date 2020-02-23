10e69234ba2d4f081497e73b5ebd574178541394
George Utsin <georgeutsin@gmail.com>
2020-02-22 19:42:32 -0500

    rails: add discrete attribute sample model (#35)

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_22_201249) do
+ActiveRecord::Schema.define(version: 2020_02_23_003534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2020_02_22_201249) do
     t.string "first", null: false
     t.string "second", null: false
     t.string "parent_category", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "discrete_attribute_samples", force: :cascade do |t|
+    t.string "option"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
