be84a93fa3a1ab9587647055d696df8d37bfdeaa
George Utsin <georgeutsin@gmail.com>
2020-02-24 15:38:25 -0500

    rails: update tag endpoint to support new tag types

--- rails/app/controllers/v1/tags_controller.rb
+++ rails/app/controllers/v1/tags_controller.rb
@@ -17,13 +17,9 @@ class V1::TagsController < ApplicationController
   def tags_for(project_id)
     Task.joins(:sample)
         .where(tags_filter, project_id)
-        .order(tasks_ordering)
+        .order(created_at: :asc)
         .map do |task|
-          type = task.actable_type
-          task = task.specific
-          sample = Sample.where(task_id: task.id, is_tag: true).first.specific
-          media = Medium.find(task.media_id)
-          { type: type, task: task, tag: sample, media: media }
+          tag_for(task)
         end
   end
 
@@ -40,13 +36,47 @@ class V1::TagsController < ApplicationController
   def tags_filter
     <<-SQL
       tasks.project_id = ?
+      AND tasks.parent_id IS NULL
       AND samples.is_tag
     SQL
   end
 
-  def tasks_ordering
-    <<-SQL
-      tasks.created_at ASC
-    SQL
+  # rubocop:disable Metrics/MethodLength
+  def tag_for(task)
+    tag = nil
+    case task.actable_type
+    when 'BoundingBoxTask', 'LocatorTask', 'DiscreteAttributeTask'
+      tag = tag_for_basic(task)
+    when 'DichotomyTask'
+      tag = tag_for_dichotomy(task)
+    when 'MetadataTask'
+      tag = tag_for_metadata(task)
+    else
+      return json_error(message: 'unknown task type')
+    end
+    base_tag_params(task).merge({ tag: tag })
+  end
+  # rubocop:enable Metrics/MethodLength
+
+  def base_tag_params(task)
+    type = task.actable_type
+    task = task.specific
+    media = Medium.find(task.media_id)
+    { type: type, task: task, media: media }
+  end
+
+  def tag_for_basic(task)
+    Sample.find_by(task_id: task.id, is_tag: true).specific
+  end
+
+  def tag_for_dichotomy(task)
+    bb_task = Task.find(actable_type: 'BoundingBoxTask', parent_id: task.id)
+    metadata_task = Task.find(actable_type: 'MetadataTask', parent_id: task.id)
+    { bounding_box: tag_for_basic(bb_task), metadata: tag_for_metadata(metadata_task) }
+  end
+
+  def tag_for_metadata(task)
+    da_tasks = Task.where(actable_type: 'DiscreteAttributeTask', parent_id: task.id)
+    Sample.find_by(task_id: da_tasks.map(&:id), is_tag: true)
   end
 end
