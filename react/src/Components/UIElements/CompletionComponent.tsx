feb3deedc7fa5e82a7e31343c4f80afc68406835
George Utsin <georgeutsin@gmail.com>
2020-02-26 13:08:25 -0500

    add confetti

--- react/src/Components/UIElements/CompletionComponent.tsx
+++ react/src/Components/UIElements/CompletionComponent.tsx
@@ -1,23 +1,40 @@
+import confetti from "canvas-confetti";
 import React, { Component } from "react";
 import "./Completion.css";
 
 const completionTexts: string[] = [
     "That's it!",
-    "You're done!",
-    "You're finished!",
-    "Guess we're done here",
-    "Guess that's all",
+    "Done!",
+    "Finished!",
+    "That's all!",
+    "Nice!",
 ];
 
 class CompletionComponent extends Component<any, any> {
     public render() {
-        const rand = Math.floor(Math.random() * Math.floor(completionTexts.length - 1));;
+        confetti({
+            particleCount: 100,
+            spread: 80,
+            angle: 225,
+            colors: ["#6100ff", "#a332ff"],
+            origin: { x: 1, y: -0.3 },
+        });
+
+        confetti({
+            particleCount: 100,
+            spread: 80,
+            angle: 315,
+            colors: ["#6100ff", "#a332ff"],
+            origin: { x: 0, y: -0.3 },
+        });
+        const rand = Math.floor(Math.random() * Math.floor(completionTexts.length - 1)); ;
         return <div className="fullscreenOverlay">
             <div className="vertical-center">
                 <div style={{ padding: 10 }}>
                     <h1 className="completion">
                         {completionTexts[rand]}
                     </h1>
+                    <br></br>
                     <div>
                         Tap anywhere to claim your reward.
                 </div>
