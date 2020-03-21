78932317deebb8981316a21a59f094e7acfb5a44
Kevin Peng <kpeng97@gmail.com>
2020-03-21 14:55:24 -0400

    react: take project id as arg in infinite view (#100)
    * take project id as arg
    
    * change actor id

--- react/src/Components/Views/ActivitiesInfiniteView.tsx
+++ react/src/Components/Views/ActivitiesInfiniteView.tsx
@@ -33,8 +33,8 @@ class ActivitiesInfiniteView extends React.Component<any, IActivitiesInfiniteVie
             completedActivityCounter: 0,
             // TODO: load number of activities in the current session dynamically from the BE, based on user trust
             numActivities: 3,
-            deviceId: values.device_id ? String(values.device_id) : getActorSig("web_player"),
-            projectId: "9",
+            deviceId: values.device_id ? String(values.device_id) : getActorSig("web_infinite"),
+            projectId: values.project_id ? String(values.project_id) : "9",
             waitingOnPost: false,
         };
 
