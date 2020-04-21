d6c6fc11b6bd5aa5c0a3c0cca70927c82af4e86b
George Utsin <georgeutsin@gmail.com>
2020-04-21 14:25:16 -0400

    react: alphabetize exports

--- react/src/components/canvases/index.js
+++ react/src/components/canvases/index.js
@@ -1,8 +1,8 @@
 export { default as BaseCanvas } from "./BaseCanvas";
-export { default as BoundingBoxCreationCanvas } from "./BoundingBoxCreationCanvas";
 export { default as BoundingBoxCanvas } from "./BoundingBoxCanvas";
-export { default as PointCreationCanvas } from "./PointCreationCanvas";
+export { default as BoundingBoxCreationCanvas } from "./BoundingBoxCreationCanvas";
 export { default as PointCanvas } from "./PointCanvas";
+export { default as PointCreationCanvas } from "./PointCreationCanvas";
 export {
     drawMarker,
     drawGreenMarker,
