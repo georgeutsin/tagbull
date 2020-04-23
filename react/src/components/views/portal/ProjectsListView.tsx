84c2eb649cd322fd1691255644a74e086e88d1d9
George Utsin <georgeutsin@gmail.com>
2020-04-23 16:39:44 -0400

    react: refactor portal to common wrapper

--- react/src/components/views/portal/ProjectsListView.tsx
+++ react/src/components/views/portal/ProjectsListView.tsx
@@ -1,33 +1,22 @@
 import React, { Component } from "react";
-import { NavBar, ProjectsList } from "../../elements";
+import { PortalWrapper, ProjectsList } from "../../elements";
 
 import portalStyles from "../../../styles/portal.module.scss";
 
 class ProjectsListView extends Component {
     public render() {
-        return <div>
-            <NavBar isPortal>
-                <li>
-                    <a href="/about">About Us</a>
-                </li>
-            </NavBar>
-            <div className={portalStyles.portalWrapper} style={{ minHeight: "100vh" }}>
-                <div className="spacer"></div>
-                <div className={portalStyles.actionBar}>
-                    <span style={{ display: "inline-block" }}><h1>Projects</h1></span>
-                    <span className={portalStyles.actions}>
-                        <a href="/projects/new"><button className={portalStyles.actionButton}>
-                            New Project
-                        </button></a>
-                    </span>
-                    <div style={{ clear: "both" }}></div>
-                </div>
-
-                <div className={portalStyles.mainCard}>
-                    <ProjectsList></ProjectsList>
-                </div>
-            </div>
-        </div>;
+        const actions = <span className={portalStyles.actions}>
+            <a href="/projects/new">
+                <button className={portalStyles.actionButton}>
+                    New Project
+                </button>
+            </a>
+        </span>;
+        return <PortalWrapper
+            title={`Projects`}
+            actions={actions}>
+            <ProjectsList></ProjectsList>
+        </PortalWrapper>;
     }
 }
 
