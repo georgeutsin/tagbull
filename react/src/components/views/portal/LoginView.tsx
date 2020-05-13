4245b0f2e15851cb08e0cc2b52eca7d0c810c15d
George Utsin <georgeutsin@gmail.com>
2020-05-13 13:29:39 -0400

    react: add login button to navbar on homepage (#153)

--- react/src/components/views/portal/LoginView.tsx
+++ react/src/components/views/portal/LoginView.tsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
-import { LoginForm, PortalWrapper } from "../../elements";
+import { Footer, LoginForm, NavBar } from "../../elements";
+
+import portalStyles from "../../../styles/portal.module.scss";
 
 class LoginView extends Component {
     public render() {
-        return <PortalWrapper
-            actions={null}
-            maxWidth={400}>
-                <LoginForm></LoginForm>
-        </PortalWrapper>;
+        return <div>
+            <NavBar>
+            </NavBar>
+            <div className={portalStyles.portalWrapper} style={{ minHeight: "100vh" }}>
+                <div className="spacer"></div>
+                <div className={portalStyles.mainCard} style={{ maxWidth: 400 }}>
+                    <LoginForm></LoginForm>
+                </div>
+            </div>
+            <div className="spacer"></div>
+            <Footer></Footer>
+        </div>;
     }
 }
 
