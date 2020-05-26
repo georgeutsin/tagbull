4f8f3fe614384824a89b9cd1afab7c16b2b43964
George Utsin <georgeutsin@gmail.com>
2020-05-26 12:33:08 -0400

    backend: reomve gcloud dependencies

--- rails/config/environments/development.rb
+++ rails/config/environments/development.rb
@@ -50,8 +50,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.google_cloud.use_trace = false
-  config.google_cloud.use_logging = false
-  config.google_cloud.use_error_reporting = false
-  config.google_cloud.use_debugger = false
 end
